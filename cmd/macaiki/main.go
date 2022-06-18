fa7143ed363cdb51cc3e1603e098788b8cddb93c
restuarachman <restuarachman@gmail.com>
2022-06-19 02:21:15 +0700

    using pull images

--- cmd/macaiki/main.go
+++ cmd/macaiki/main.go
@@ -37,7 +37,7 @@ func main() {
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
+		return c.String(http.StatusOK, "Hello Dunia!")
 	})
 	v := validator.New()
 
